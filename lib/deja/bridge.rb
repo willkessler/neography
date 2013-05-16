f3fc6039ee60f25aa7872206dac4cb96bc5fd565
Kamran Pakseresht <kamran@crunchbase.com>
2013-05-16 16:10:29 -0700

    pulling node fetchers out into methods

--- lib/deja/bridge.rb
+++ lib/deja/bridge.rb
@@ -10,42 +10,49 @@ module Deja
 
       end
 
-      def load_entity(neo_id, relations = :all)
-        case relations
-        when :all
-          @neo.execute_cypher(neo_id) do |start_node|
-            start_node(neoid).ret <=> node.ret
-          end
-          break
-        when :outgoing
-          @neo.execute_cypher(neo_id) do |start_node|
-            start_node(neoid).ret >> node.ret
-          end
-          break
-        when :incoming
-          @neo.execute_cypher(neo_id) do |start_node|
-            start_node(neoid).ret << node.ret
-          end
-          break
-        when :none
-          @neo.execute_cypher(neo_id) do |start_node|
-            start_node
-          end
-          break
-        # Assumes array of relationship types - should integrate type + directionality in future
-        when Array
-          @neo.execute_cypher(neo_id) do |start_node|
-            start_node(neo_id).ret - relations.join('|').ret - node.ret
-          end
-          break
-        else
-          if(relations.is_a?(Symbol))
-            @neo.execute_cypher(neo_id) do |start_node|
-              start_node(neo_id).ret - relations.ret - node.ret
-            end
-          else
-            raise Deja::Error::InvalidParameter
-          end
+      def load_entity(neo_id, *relations = :all)
+        get_node_with_relationships(neo_id, relations) if relations.is_a? Array
+        raise Deja::Error::InvalidParameter unless relations.is_a? Symbol
+        get_all_related_nodes(neo_id) if relations == :all
+        get_all_outgoing_nodes(neo_id) if relations == :outgoing
+        get_all_incoming_nodes(neo_id) if relations == :incoming
+        get_single_node(neo_id) if relations == :none
+        get_node_with_relationship(neo_id)
+      end
+
+      def get_all_related_nodes(neo_id)
+        @neo.execute_cypher(neo_id) do |start_node|
+          start_node(neoid).ret <=> node.ret
+        end
+      end
+
+      def get_all_outgoing_nodes(neo_id)
+        @neo.execute_cypher(neo_id) do |start_node|
+          start_node(neoid).ret >> node.ret
+        end
+      end
+
+      def get_all_incoming_nodes(neo_id)
+        @neo.execute_cypher(neo_id) do |start_node|
+          start_node(neoid).ret << node.ret
+        end
+      end
+
+      def get_single_node(neo_id)
+        @neo.execute_cypher(neo_id) do |start_node|
+          start_node
+        end
+      end
+
+      def get_node_with_relationship(neo_id, relationship)
+        @neo.execute_cypher(neo_id) do |start_node|
+          start_node(neo_id).ret - relationship.ret - node.ret
+        end
+      end
+
+      def get_node_with_relationships(neo_id, *relations)
+        @neo.execute_cypher(neo_id) do |start_node|
+          start_node(neo_id).ret - relations.join('|').ret - node.ret
         end
       end
 
