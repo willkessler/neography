d17bc93d77533c8c54f3343670e0b9d8f0929ce2
Anthony W. Nguyen <anwnguyen@gmail.com>
2013-05-23 17:04:03 -0700

    Fixes the neo4j via neography initialization for testing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,8 +9,12 @@ include Deja
 # Start Neo4j server
 begin
   @neo = Neography::Rest.new(:directory => '/deja-test')
+  # @neo.connection.get('/deja-test') # attempts get request
+  @neo.connection.get('/deja-test')
+rescue Errno::ECONNREFUSED => e
+  cmd = ''
+  cmd << 'rake neo4j:install;' unless File.exists?('neo4j')
+  cmd << 'rake neo4j:start; echo Neo4j has started. Please re-run rspec. Note: to stop neo4j, run \"rake neo4j:stop\"'
+  exec(cmd)
 rescue Neography::NeographyError
-  exec('rake neo4j:start') # if @neo.get_root[:reference_node]
-  # Sets the test Neography connection
-  @neo = Neography::Rest.new(:directory => '/deja-test')
 end
