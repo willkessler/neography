662dc308136aa8ba01eed141fdfc026bf5b3b0f5
Kamran <kamran@crunchbase.com>
2013-06-05 14:54:22 -0700

    maintenance push, base work for fetching related nodes and embedding in node objects

--- lib/deja/finders.rb
+++ lib/deja/finders.rb
@@ -5,18 +5,31 @@ module Deja
     module ClassMethods
       def load(*ids)
         nodes = ids.map do |id|
-          sane_hash = sane_hash(load_entity(id))
-
-          node_instance = self.new(sane_hash.first)
-          rel_instance = Deja::Relationship.new(sane_hash[1][:type], :both, self.new(sane_hash[2]), self)
-          node_instance.relationships.merge!(rel_instance.label.to_sym => rel_instance)
-          node_instance
+          entity_hash = sane_hash(load_entity(id))
+          initial_node = self.new(entity_hash.first)
+          related_nodes = Array.new
+          entity_hash.each do |entity|
+            if entity.has_key?(:type)
+              related_node = self.new(sane_hash(load_entity(entity[:start], :none)).first)
+              eval("initial_node.set_#{entity[:type]}(:#{entity[:type]}, :both, initial_node, related_node)")
+              related_nodes << related_node.id
+            else
+              unless entity[:id] == initial_node.id || related_nodes.include?(entity[:id])
+                related_node = self.new(sane_hash(load_entity(entity[:id], :none)))
+              end
+            end
+          end
+          initial_node
         end
         if ids.length == 1
           nodes.first
         else
           nodes
         end
+      end
+
+      def load_single(id)
+        self.new()
       end
 
       def find(*args, &block)
